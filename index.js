c96ea4336c49d159e877a99a9edf1d8044f0cfd3
Dipak Dhariyaparmar <152061206+Dipak-Dhariyaparmar@users.noreply.github.com>
2025-05-20 09:18:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 console.log("Hello I am contributer for created by mr.Dipak!");
 
+console.log("Hello Today is a 10 may");
 console.log("Hello Today is a 11 may");
+console.log("Hello Today is a 12 may");
+console.log("Hello Today is a 13 may");
+console.log("Hello Today is a 14 may");
+console.log("Hello Today is a 15 may");
+console.log("Hello Today is a 16 may");
+console.log("Hello Today is a 17 may");
+console.log("Hello Today is a 18 may");
+console.log("Hello Today is a 19 may");
+console.log("Hello Today is a 20 may");
